5492493831d7b0819d5f6e0f467089ef3c5e1e26
Jose piedra <j.piedraprog@gmail.com>
2023-02-16 23:23:58 -0400

    calculadora basica en python

--- practica-1/primer-practica.py
+++ practica-1/primer-practica.py
@@ -1,1 +1,38 @@
-print("fuap")
+from os import system
+
+salida = False 
+
+while salida == False : 
+    print('Bienvenido a la calculadora')
+    print('para hacer las operaciones,\n selecciona un numero')
+    print('para eso tienes que introducir un numero en consola y darle enter')
+    print('-------------------------------------------------') 
+    print('1.- Suma dos números') 
+    print('2.- Resta dos números') 
+    print('3.- Multiplica dos números') 
+    print('4.- salir') 
+
+    operacion = int(input('Elije la opcion: '))
+    if operacion == 1 :
+        system('cls') 
+        n1 = int(input('ingrese el primer numero: '))
+        n2 = int(input('ingrese el segundo numero: '))
+        print("el resultado de la suma es: ",n1 + n2)
+
+        
+    if operacion == 2:
+        system('cls') 
+        n1 = int(input('ingrese el primer numero: '))
+        n2 = int(input('ingrese el segundo numero: '))
+        print("el resultado de la resta es:",n1 - n2)
+    if operacion == 3:
+        system('cls') 
+        n1 = int(input('ingrese el primer numero: '))
+        n2 = int(input('ingrese el segundo numero: '))
+        print("el resultado de la multiplicación es: ",n1 * n2)
+
+    if operacion == 4:
+        system('cls') 
+        salida = True
+    
+    
